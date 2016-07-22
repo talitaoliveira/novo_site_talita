152ca69e902bc7d8ddf94c0035865f5d3d377ff8
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2016-07-22 17:39:23 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<head>
+	<title>TESTE</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--[if lt IE 9]>
+	<script src="lib/html5shim.js"></script>
+	<![endif]-->
+
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="lib/plugins.css" type="text/css" />
+
+	<!-- Add jQuery library -->
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+	<!-- Add Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="lib/bootstrap.css">
+
+	<!-- Add All-Animation -->
+	<link rel="stylesheet" type="text/css" href="lib/all-animation.css">
+
+	<!-- Swipper JS -->
+	<script type="text/javascript" src="lib/swipper.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="lib/swipper.css">
+
+	<!-- CSS e JS da página -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="lib/script.js"></script>
+
+	<script src="lib/bootstrap.js"></script>
+
+</head>
+<body data-spy="scroll" data-target="#navbar">
+	<div id="interface">
+	<header>
+		<nav class="navbar navbar-default nav navbar-fixed-top" id="navbar">
+		  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Talita Oliveira</a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav navbar-right menu">
+		        <li class="active">
+		        	<a href="#home" class="item-menu">Home</a>
+		        </li>
+		        <li>
+		        	<a href="#sobre" class="item-menu">Sobre</a>
+		        </li>
+		         <li>
+		        	<a href="#portfolio" class="item-menu">Portfolio</a>
+		        </li>
+		        <li>
+		        	<a href="#contato" class="item-menu">Contato</a>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</header>
+	<div class="container-principal">
+		<section id="home" class="bg-verde">
+			<!-- <h1 class="h1-branco">Home</h1> -->
+				<h1 class="h1-home olao">&#60;/talita&#62;</h1>
+		</section>
+		<section id="sobre" class="bg-branco ">
+			<h1 class="h1-verde">Sobre</h1>
+			<section class="container">
+				<section class="col-md-4 item-skills"><h2>Skills</h2></section>
+				<section class="col-md-8 item-skills">
+					<div class="col-md-2 skill">
+						<figure>
+							<img src="icons/html5-128.png">
+						</figure>
+					</div>
+					<div class="col-md-2 skill">
+						<figure>
+							<img src="icons/css3-128.png">
+						</figure>
+					</div>
+					<div class="col-md-2 skill">
+						<figure>
+							<img src="icons/php-logo-128.png">
+						</figure>
+					</div>
+					<div class="col-md-2 skill">
+						<figure>
+							<img src="icons/cakephp.png">
+						</figure>
+					</div>
+					<div class="col-md-2 skill">
+						NULL
+					</div>
+					<div class="col-md-2 skill">
+						NULL
+					</div>
+				</section>
+			</section>
+		</section>
+		<section id="portfolio" class="bg-verde">
+			<h1 class="h1-branco">Portfolio</h1>
+			<section id="lista-portfolio" class="container">
+				<section class="item-portfolio col-sm-3">
+					<figure>
+						<img src="img/img1.jpg">
+					</figure>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint officia rem facilis quas molestias mollitia facere ipsam maxime suscipit vel laudantium itaque possimus numquam sunt hic, ipsa ducimus minus asperiores!</p>
+				</section>
+				<section class="item-portfolio col-sm-3">
+					<figure>
+						<img src="img/img2.jpg">
+					</figure>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint officia rem facilis quas molestias mollitia facere ipsam maxime suscipit vel laudantium itaque possimus numquam sunt hic, ipsa ducimus minus asperiores!</p>
+				</section>
+				<section class="item-portfolio col-sm-3">
+					<figure>
+						<img src="img/img3.jpg">
+					</figure>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint officia rem facilis quas molestias mollitia facere ipsam maxime suscipit vel laudantium itaque possimus numquam sunt hic, ipsa ducimus minus asperiores!</p>
+				</section>
+				<section class="item-portfolio col-sm-3">
+					<figure>
+						<img src="img/img4.jpg">
+					</figure>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint officia rem facilis quas molestias mollitia facere ipsam maxime suscipit vel laudantium itaque possimus numquam sunt hic, ipsa ducimus minus asperiores!</p>
+				</section>
+			</section>
+		</section>
+		<section id="contato" class="bg-branco">
+			<h1 class="h1-verde">Contato</h1>
+		</section>
+		<footer>
+			<div id="inicio-footer">
+				Facebook<br>
+			</div>
+			<div id="fim-footer">
+				<div id="div-copyright">Copyright © Talita Oliveira 2016</div>
+			</div>
+		</footer><div id="toTop">Voltar ao topo</div>
+	</div>
+	</body>
+</html>
+
+
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$(".h1-home").click(function(){
+		$(this).addClass("alor");
+	});
+});
